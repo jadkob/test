78a7a47d1ec657fb7a0af6de6bb11b2c9debf7d7
stapler-codes <jadkoneissi@gmail.com>
2024-03-27 21:50:28 +0200

    test2

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,3 +1,10 @@
+import axios from "axios"
+import {useEffect} from "react";
 export default function Home() {
+    useEffect(() => {
+        axios.get("/api").then((res) => {
+            console.log(res.data)
+        })
+    }, [])
     return <h1>test</h1>
 }
